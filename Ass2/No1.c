9d92ba7235ae07ed56b7119dfc5dc95e3844919e
Avishek Mondal <33727239+AvishekGhost@users.noreply.github.com>
2019-01-28 21:25:02 +0530

    Update No1.c

--- Ass2/No1.c
+++ Ass2/No1.c
@@ -2,6 +2,7 @@
 #include<math.h>
 #define MAX 3
 int main(){
+	float e1,e2,e3, o1,o2,o3, n1,n2,n3;
 	float aL[MAX][MAX], aR[MAX],x[MAX];
 	int i = 0,j=0;
 	printf("Enter The elements For the LEFT SIDE:\n");
@@ -17,27 +18,32 @@ int main(){
 	printf("Printing --\n");
 	for(i=0;i<MAX;i++){
 		for(j=0;j<MAX;j++){
-			printf("%f ",aL[i][j]);
+			printf("%.2f ",aL[i][j]);
 		}
 		if(i == MAX/2 ){
-			printf("= %f",aR[i]);
+			printf("= %.2f",aR[i]);
 		}
-		else printf("  %f",aR[i]);
+		else printf("  %.2f",aR[i]);
 		printf("\n");
 	}
     printf("Enter Initial value of X1 X2 and X3 respectively : ");
     for (i = 0; i < MAX; i++) {
         scanf("%f", &x[i]);
     }
-    printf("iter\tX1\t\t\tX2\t\t\t X3\n");
+    printf("iter\tX1\t\t\tE1\t\t\tX2\t\t\tE2\t\t\tX3\t\t\tE3\n");
 	for (i = 0; i < 6; ++i){
+		o1 = x[0]; o2 = x[1]; o3 = x[2];
+
         x[0] = (aR[0] - (aL[0][1]*x[1]) - (aL[0][2] * x[2]) )/aL[0][0];
 		x[1] = (aR[1] - (aL[1][0]*x[0]) - (aL[1][2] * x[2]) )/aL[1][1];
 		x[2] = (aR[2] - (aL[2][0]*x[0]) - (aL[2][1] * x[1]) )/aL[2][2];
 
-        printf("%d\t%.4f\t\t%.4f\t\t%.4f\n",i+1,x[0],x[1],x[2]);
+		n1 = x[0]; n2 = x[1]; n3 = x[2];
+		e1 = ((n1-o1)/n1)*100;
+		e2 = ((n2-o2)/n2)*100;
+		e3 = ((n3-o3)/n3)*100;
+        printf("%d\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\n",i+1,x[0],e1,x[1],e2,x[2],e3);
 	}
 
     return 0;
 }
-.
