5165c12a43d079fa626848b6944a44fc7bb26b57
Avishek Mondal <33727239+AvishekGhost@users.noreply.github.com>
2019-03-18 21:45:48 +0530

    Update No1.c

--- Ass3/No1.c
+++ Ass3/No1.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-#include <math.h>
+#define SIZE 20
 
-float f(float x){
-	return ( ( 2000 * log ( 140000/(140000-2100*x))) - (9.8*x));
+float function(float x){
+        return (x*x + 1);
 }
 
 int main(){
-	float a,b;
+        int i;
+        float a, b, n, h, sum = 0, ans;
+        printf("Enter the value of a(lower limit): ");
+        scanf("%f",&a);
+        printf("\nEnter the value of b(upper limit): ");
+        scanf("%f",&b);
+        printf("\nEnter the value of n(no. of segments): ");
+        scanf("%f",&n);
 
-	scanf("%f%f",&a,&b);
-	float intg = (b-a)*( (f(a)+f(b)) / 2 );
-	printf("%f\n",intg);
+        h = (b-a)/n;
+        for(i=1;i<n;i++){
+                sum += function(a + (i*h));
+        }
 
-	return 0;
+        ans = h/2 * (function(a) + function(b) + (2 * sum));
+        printf("\nAnswer: %f", ans);
+
+        return 0;
 }
